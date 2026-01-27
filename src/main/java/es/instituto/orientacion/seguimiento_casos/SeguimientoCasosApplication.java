992233792aaf4f0065ba65217137aa4b45d71729
Martin <martinhnandezfnandez@gmail.com>
2026-01-27 14:19:07 +0100

    fix- El formulario ahora registra

--- src/main/java/es/instituto/orientacion/seguimiento_casos/SeguimientoCasosApplication.java
+++ src/main/java/es/instituto/orientacion/seguimiento_casos/SeguimientoCasosApplication.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 @SpringBootApplication
 public class SeguimientoCasosApplication {
-
+//
 
     public static void main(String[] args) {
 		SpringApplication.run(SeguimientoCasosApplication.class, args);
