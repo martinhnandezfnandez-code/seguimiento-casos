e15c45d5196c5364f4e60455164a5929d938a8d3
Martin <martinhnandezfnandez@gmail.com>
2026-01-30 19:47:16 +0100

    fix- Cambiados los htmls para adptarlos a la empresa

---
+++ src/main/java/es/instituto/orientacion/seguimiento_casos/services/servicesimpl/ListarServiceImpl.java
@@ -0,0 +1,6 @@
+package es.instituto.orientacion.seguimiento_casos.services.servicesimpl;
+
+import es.instituto.orientacion.seguimiento_casos.services.ListarService;
+
+public class ListarServiceImpl implements ListarService {
+}
